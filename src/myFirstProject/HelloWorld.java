1075609e0828244f88120a6f2b231ac93422953d
classroom <classroom@yccc.edu>
2018-09-05 19:27:02 -0400

    first commit

---
+++ src/myFirstProject/HelloWorld.java
@@ -0,0 +1,15 @@
+package myFirstProject;
+
+public class HelloWorld {
+
+    // An object is 2 things.
+	// 1. Actions. Methods.
+	// 2. Attributes. Variables.
+private String name = "Mike";
+	
+// main method. Starts the program.
+public static void main(String []args)
+{
+// Prints to the console.
+	
+	
